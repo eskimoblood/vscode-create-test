0fce25bdd0df980ca428ee28b061ce14c66af719
Parker, Chris - 2 <chris.parker1@ml.com>
2018-08-12 18:07:48 -0400

    Further revisions to support testFileExtension setting, new soureFileExtension setting.

--- src/pathUtils.ts
+++ src/pathUtils.ts
@@ -12,38 +12,46 @@ const pathFromUri = ({ fsPath }: Uri) => filepath.create(fsPath)
 
 const unixifyPath = (path) => (slashChar == "\\") ? path.replace(/\\/g, "/") : path
 
-const relativePath = (uri, config) => {
+const isTestModule = ({ uri, workSpaceUri, config }) => {
+    const path = uri.fsPath.replace(workSpaceUri.fsPath, '')
+    return isRelativeFolder(config)
+        ? filepath.create(filepath.create(path).dirname()).basename() == config.testFolder.substr(2)
+        : path.startsWith(slashChar + config.testFolder)
+}
+
+const relativePath = (uri: Uri, workSpaceUri: Uri, config) => {
 	const path = pathFromUri(uri).dir()
-	const isTestModule = (path.basename() == config.testFolder.substr(2))
-	return isTestModule ? path.dir() : path.append(config.testFolder.replace('./', ''))
+	return isTestModule(uri, workSpaceUri, config) ? path.dir() : path.append(config.testFolder.replace('./', ''))
 }
 
 const absolutePath = (uri: Uri, workSpaceUri: Uri, config) => {
     let path = uri.fsPath.replace(workSpaceUri.fsPath, '')
-    const isTestModule = path.startsWith(slashChar + config.testFolder)
-    if (isTestModule)
+	const _isTestModule = isTestModule(uri, workSpaceUri, config)
+    if (_isTestModule)
         path = path.replace(new RegExp(`^\\${slashChar}${config.testFolder}\\${slashChar}`), (config.srcFolder ? slashChar + config.srcFolder : '') + slashChar)
     else if (config.srcFolder)
         path = path.replace(new RegExp(`^\\${slashChar}${config.srcFolder}\\${slashChar}`), slashChar)
     var retval = pathFromUri(workSpaceUri)
-    if (!isTestModule)
+    if (!_isTestModule)
         retval = retval.append(config.testFolder)
     return retval
         .append(path)
         .dir()
 }
 
-const pathToTestFolder = (uri: Uri, workSpaceUri: Uri, config) =>
+const pathToNewFolder = (uri: Uri, workSpaceUri: Uri, config) =>
     isRelativeFolder(config)
-        ? relativePath(uri, config)
+        ? relativePath(uri, workSpaceUri, config)
         : absolutePath(uri, workSpaceUri, config)
 
-const name = p => p.basename(p.extname())
+const name = (p, ext) => p.basename(ext)
 
-export const pathToFile = (uri: Uri, workSpaceUri: Uri, config) =>
-    pathToTestFolder(uri, workSpaceUri, config).append(
-        name(pathFromUri(uri)) + config.testFileExtension
-    )
+export const pathToFile = (uri: Uri, workSpaceUri: Uri, config) => {
+    const _isTestModule = isTestModule(uri, workSpaceUri, config);
+    const basename = name(pathFromUri(uri), _isTestModule ? config.testFileExtension : config.sourceFileExtension);
+    const extname = _isTestModule ? config.sourceFileExtension : config.testFileExtension;
+    return pathToNewFolder(uri, workSpaceUri, config).append(basename + extname);
+    }
 
 const getTemplate = (config: {
     testFileTemplate: Array<String> | Object
@@ -63,7 +71,7 @@ const getRelativePath = (p1, p2) =>
 export const createFile = (file, uri, workSpaceUri, config): Thenable<any> => {
     const filePath = filepath.create(uri.fsPath)
     const relativePath = getRelativePath(file, filePath)
-    const moduleName = name(filePath)
+    const moduleName = name(filePath, config.sourceFileExtension)
     const modulePath = relativePath.replace(filePath.extname(), '')
     return getTemplate(config).then(template =>
         file.write(
